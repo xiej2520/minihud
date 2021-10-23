cae52e0bda3039ad1163d8c21129b522d17319f9
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2021-10-23 18:33:25 +0300

    Fix the debug renderers not activating properly from the toggle configs

--- src/main/java/fi/dy/masa/minihud/util/DebugInfoUtils.java
+++ src/main/java/fi/dy/masa/minihud/util/DebugInfoUtils.java
@@ -1,9 +1,9 @@
 package fi.dy.masa.minihud.util;
 
-import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
+import io.netty.buffer.Unpooled;
 import com.google.common.collect.MapMaker;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
@@ -26,12 +26,11 @@ import net.minecraft.text.LiteralText;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+import fi.dy.masa.malilib.config.IConfigBoolean;
 import fi.dy.masa.minihud.config.Configs;
 import fi.dy.masa.minihud.config.RendererToggle;
 import fi.dy.masa.minihud.mixin.IMixinEntityNavigation;
-import io.netty.buffer.Unpooled;
 
 public class DebugInfoUtils
 {
@@ -216,7 +215,7 @@ public class DebugInfoUtils
         return false;
     }
 
-    public static void toggleDebugRenderer(RendererToggle config)
+    public static void toggleDebugRenderer(IConfigBoolean config)
     {
         if (config == RendererToggle.DEBUG_NEIGHBOR_UPDATES)
         {
