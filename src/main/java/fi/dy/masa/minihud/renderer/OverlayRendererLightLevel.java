bcebdcdad4bc1c387df05f4068192da68230d003
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2022-01-04 22:28:30 -0600

    Light Level Overlay: Add an option to not render under water (under fluids)

--- src/main/java/fi/dy/masa/minihud/renderer/OverlayRendererLightLevel.java
+++ src/main/java/fi/dy/masa/minihud/renderer/OverlayRendererLightLevel.java
@@ -330,6 +330,7 @@ public class OverlayRendererLightLevel extends OverlayRendererBase
         BlockPos.Mutable mutablePos = new BlockPos.Mutable();
         final int worldTopHeight = world.getHeight();
         final boolean collisionCheck = Configs.Generic.LIGHT_LEVEL_COLLISION_CHECK.getBooleanValue();
+        final boolean underWater = Configs.Generic.LIGHT_LEVEL_UNDER_WATER.getBooleanValue();
         final boolean autoHeight = Configs.Generic.LIGHT_LEVEL_AUTO_HEIGHT.getBooleanValue();
 
         for (int cx = minCX; cx <= maxCX; ++cx)
@@ -360,7 +361,8 @@ public class OverlayRendererLightLevel extends OverlayRendererBase
                             mutablePos.set(x, y, z);
                             BlockState state = chunk.getBlockState(mutablePos);
 
-                            if (collisionCheck == false || state.getCollisionShape(chunk, mutablePos).isEmpty())
+                            if ((collisionCheck == false || state.getCollisionShape(chunk, mutablePos).isEmpty()) &&
+                                (underWater || state.getFluidState().isEmpty()))
                             {
                                 int block = y < worldTopHeight ? lightingProvider.get(LightType.BLOCK).getLightLevel(mutablePos) : 0;
                                 int sky   = y < worldTopHeight ? lightingProvider.get(LightType.SKY).getLightLevel(mutablePos) : 15;
