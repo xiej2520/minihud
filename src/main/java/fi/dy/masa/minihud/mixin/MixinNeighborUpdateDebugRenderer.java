20f5f845d1fa9a5cd17772b9575a07d7ddfc36c8
xiej2520 <jackyxie2520@outlook.com>
2025-05-25 16:02:03 -0500

    Add a really bad hack fix for NeigborUpdateDebugRenderer.
    1.15 doesn't call it with matrixstack, which draws the boxes in the wrong place.
    Also calling vertexConsumers.getBuffer() draws random entities in the wrong place.

--- src/main/java/fi/dy/masa/minihud/mixin/MixinNeighborUpdateDebugRenderer.java
+++ src/main/java/fi/dy/masa/minihud/mixin/MixinNeighborUpdateDebugRenderer.java
@@ -1,8 +1,13 @@
 package fi.dy.masa.minihud.mixin;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import fi.dy.masa.malilib.util.Color4f;
+import net.minecraft.client.render.*;
+import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import fi.dy.masa.minihud.event.RenderHandler;
 import net.minecraft.client.render.debug.NeighborUpdateDebugRenderer;
@@ -14,5 +19,26 @@ public abstract class MixinNeighborUpdateDebugRenderer
     public void fixDebugRendererState(CallbackInfo ci)
     {
         RenderHandler.fixDebugRendererState();
+    }
+
+    // very bad hack fix
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/VertexConsumerProvider;getBuffer(Lnet/minecraft/client/render/RenderLayer;)Lnet/minecraft/client/render/VertexConsumer;"))
+    public VertexConsumer dontGetBuffer(VertexConsumerProvider instance, RenderLayer renderLayer) {
+        // simply calling vertexConsumers.getBuffer() causes rendering glitches?
+        return null;
+    }
+
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;drawBox(Lnet/minecraft/client/render/VertexConsumer;DDDDDDFFFF)V"))
+    public void fixDrawBox(VertexConsumer vertexConsumer, double d, double e, double f, double g, double h, double i, float j, float k, float l, float m) {
+        RenderSystem.disableTexture();
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder buffer = tessellator.getBuffer();
+
+        buffer.begin(GL11.GL_LINES, VertexFormats.POSITION_COLOR);
+
+        fi.dy.masa.malilib.render.RenderUtils.drawBoxAllEdgesBatchedLines(d, e, f, g, h, i, new Color4f(j, k, l, m), buffer);
+
+        tessellator.draw();
+        RenderSystem.enableTexture();
     }
 }
